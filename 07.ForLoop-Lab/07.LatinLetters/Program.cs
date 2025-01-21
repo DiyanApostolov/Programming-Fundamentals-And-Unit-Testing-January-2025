dc2ad115da08d627d53831f4d3d47a83e73f377d
Diyan Apostolov <diyan.apostolov@yahoo.com>
2025-01-21 10:17:09 +0200

    Add solutions - For Loop

---
+++ 07.ForLoop-Lab/07.LatinLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿// Input
+
+char startLetter = char.Parse(Console.ReadLine());
+char endLetter = char.Parse(Console.ReadLine());
+
+// Output
+
+for (char letter = startLetter; letter <= endLetter; letter++)
+{
+    Console.Write(letter + " ");
+}
+
