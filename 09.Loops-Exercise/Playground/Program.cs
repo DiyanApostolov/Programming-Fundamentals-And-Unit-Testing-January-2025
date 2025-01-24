3ac0fa04d7567257c319ca0e6d13bdd90811cb76
Diyan Apostolov <diyan.apostolov@yahoo.com>
2025-01-24 08:19:29 +0200

    Add solutions for Loops - Exercise

---
+++ 09.Loops-Exercise/Playground/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-250));
+            Console.WriteLine(Math.Floor(2.99));
+            Console.WriteLine(Math.Ceiling(2.01));
+            Console.WriteLine(Math.Max(1, 200));
+        }
+    }
+}
